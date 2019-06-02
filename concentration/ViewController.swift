57f5bef3b8225be5df326fe0bfa186ef9941cf3a
Mason J. Katz <Mason.Katz@gmail.com>
2019-06-02 15:50:15 -0700

    Initial Commit

---
+++ concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  concentration
+//
+//  Created by Mason Katz on 6/2/19.
+//  Copyright © 2019 Mason Katz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
