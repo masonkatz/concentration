0afe6601b871ac70cb6eedf0cde808285fdf1b31
Mason Katz <mjk@endobit.io>
2023-12-24 14:50:38 -0800

    feat: assignment 1

--- Concentration/ContentView.swift
+++ Concentration/ContentView.swift
@@ -7,35 +7,130 @@
 
 import SwiftUI
 
+enum Emojis {
+    case halloween
+    case transportation
+    case animals
+}
+
 struct ContentView: View {
+    let emojiMap = [
+        Emojis.halloween:
+            [
+                "👻", "🎃", "🕷", "😈", "💀", "🕸", "🧙‍♀️",
+                "🙀", "👹", "😱", "☠️", "🍭",
+            ],
+        Emojis.transportation:
+            [
+                "🚔", "🚂", "🚲", "🚗", "🏍", "✈️", "⛵️",
+                "🛵", "🛴", "🛸", "🚁", "🛶", "🚠",
+            ],
+        Emojis.animals:
+            [
+                "🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🐻",
+                "🐼", "🐻‍❄️", "🐨", "🐯", "🦁", "🐮", "🐷", "🐸", "🐵",
+            ],
+    ]
+
+    @State var emojis: [String] = []
+
+    init() {
+        var cards = emojiMap[Emojis.halloween]!
+        cards += cards
+        cards = cards.shuffled()
+
+        _emojis = State(initialValue: cards)
+    }
+
     var body: some View {
-        HStack {
-            ForEach(1...4, id: \.self) { _ in
-                CardView(isFaceUp: true, emoji: "😂")
+        VStack {
+            Text("Memorize!").font(.title)
+            Spacer()
+            ScrollView {
+                cards
+            }
+            Spacer()
+            emojiSelecter
+        }
+        .padding()
+    }
+
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 64))]) {
+            ForEach(0..<emojis.count, id: \.self) { index in
+                CardView(isFaceUp: false, content: emojis[index])
+                    .aspectRatio(2 / 3, contentMode: .fit)
             }
         }
         .foregroundColor(.orange)
-        .padding()
     }
+
+    func chooseEmojis(theme: Emojis, symbol: String, text: String) -> some View {
+        VStack {
+            Button(
+                action: {
+                    setTheme(to: theme)
+                },
+                label: {
+                    VStack {
+                        Image(systemName: symbol).font(.title)
+                        Text(text).font(.caption)
+                    }
+                })
+        }
+    }
+
+    func setTheme(to theme: Emojis) {
+        var cards = emojiMap[theme]!
+        cards += cards
+        emojis = cards.shuffled()
+    }
+
+    var chooseHalloween: some View {
+        chooseEmojis(theme: Emojis.halloween, symbol: "moon.haze.fill", text: "Halloween")
+    }
+
+    var chooseTransportation: some View {
+        chooseEmojis(theme: Emojis.transportation, symbol: "car", text: "Transportation")
+    }
+
+    var chooseAnimals: some View {
+        chooseEmojis(theme: Emojis.animals, symbol: "hare", text: "Animals")
+    }
+
+    var emojiSelecter: some View {
+        HStack {
+            Spacer()
+            chooseHalloween
+            Spacer()
+            chooseTransportation
+            Spacer()
+            chooseAnimals
+            Spacer()
+        }
+    }
+
 }
 
 struct CardView: View {
     let cornerRadius: CGFloat = 12
     let borderWidth: CGFloat = 2
-    var isFaceUp = false
-    var emoji = "😎"
+    @State var isFaceUp = false
+    let content: String
 
     var body: some View {
         ZStack {
-            if isFaceUp {
-                RoundedRectangle(cornerRadius: cornerRadius)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: cornerRadius)
-                    .strokeBorder(lineWidth: borderWidth)
-                Text(emoji).font(.largeTitle)
-            } else {
-                RoundedRectangle(cornerRadius: cornerRadius)
+            let base = RoundedRectangle(cornerRadius: cornerRadius)
+
+            Group {
+                base.foregroundColor(.white)
+                base.strokeBorder(lineWidth: borderWidth)
+                Text(content).font(.largeTitle)
             }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+        }.onTapGesture {
+            isFaceUp.toggle()
         }
     }
 }
